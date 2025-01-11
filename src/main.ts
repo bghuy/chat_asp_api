77eb0b11ea576cf9955929d8525fd709199948a3
Bạch Gia Huy <bghuy2003@gmail.com>
2025-01-11 17:31:35 +0700

    test cookie 5

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   app.enableCors({
     origin: [process.env.CLIENT_PRODUCTION_URL, process.env.CLIENT_DEVELOPMENT_URL],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
     preflightContinue: true,
   });
+  app.use(cookieParser());
   app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
       res.status(204).send();
