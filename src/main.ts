470e2a2dcc21577c7e3095e17aa784fdb1b68467
Bạch Gia Huy <bghuy2003@gmail.com>
2025-01-11 17:48:56 +0700

    test cookie 8

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+const cookieParser = require('cookie-parser');
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins = [process.env.CLIENT_PRODUCTION_URL, process.env.CLIENT_DEVELOPMENT_URL];
 
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // Cấu hình CORS với logic kiểm tra nguồn tương tự Express
+  // app.enableCors({
+  //   origin: (origin, callback) => {
+  //     if (allowedOrigins.includes(origin) || !origin) {
+  //       // Cho phép yêu cầu từ các nguồn hợp lệ và các yêu cầu không có origin (chẳng hạn như yêu cầu từ localhost trong phát triển)
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  //   credentials: true,  // Cho phép gửi cookie trong yêu cầu
+  //   preflightContinue: true,
+  // });
+  app.use(cors({
     credentials: true,
-    preflightContinue: true,
-  });
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
+  // Middleware để xử lý các yêu cầu OPTIONS (preflight request)
   app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
       res.status(204).send();
@@ -29,7 +42,7 @@ async function bootstrap() {
     }
   });
 
-  app.use(cookieParser()); 
+  app.use(cookieParser());  // Middleware cho cookie parser
   await app.listen(process.env.PORT ?? 3000);
 }
 
