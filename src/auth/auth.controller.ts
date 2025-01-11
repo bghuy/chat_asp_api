cb4d4d1a52baf9983f49f778d44972bda5d5e96b
Bạch Gia Huy <bghuy2003@gmail.com>
2025-01-11 13:54:59 +0700

    test cookie 3

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -63,6 +63,7 @@ export class AuthController {
             maxAge: refreshTokenExpiry || sevenDaysInMs,
             sameSite: process.env.SERVER_MODE === 'production' ? 'none' : 'lax'
         });
+        console.log('Response Headers (Before Sending):', res.getHeaders());
         return res.status(200).json({ message: 'Login successful', data: {access_token} });
     } 
 
