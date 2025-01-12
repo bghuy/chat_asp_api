c0de5639ebe9c17b5612a517d48a62c169222e82
Bạch Gia Huy <bghuy2003@gmail.com>
2025-01-12 09:50:12 +0700

    revert

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // const cookieParser = require('cookie-parser');
+const cors = require('cors');
 import cookieParser from 'cookie-parser';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ["https://test-deploy-flax-theta.vercel.app", process.env.CLIENT_DEVELOPMENT_URL],
-    methods: "*",
+  const allowedOrigins = [process.env.CLIENT_PRODUCTION_URL, process.env.CLIENT_DEVELOPMENT_URL];
+
+  // Cấu hình CORS với logic kiểm tra nguồn tương tự Express
+  // app.enableCors({
+  //   origin: (origin, callback) => {
+  //     if (allowedOrigins.includes(origin) || !origin) {
+  //       // Cho phép yêu cầu từ các nguồn hợp lệ và các yêu cầu không có origin (chẳng hạn như yêu cầu từ localhost trong phát triển)
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  //   credentials: true,  // Cho phép gửi cookie trong yêu cầu
+  //   preflightContinue: true,
+  // });
+  app.use(cors({
     credentials: true,
-    allowedHeaders:"*"
-   });
-   
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
+  // Middleware để xử lý các yêu cầu OPTIONS (preflight request)
   app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
       res.status(204).send();
@@ -21,7 +42,7 @@ async function bootstrap() {
     }
   });
 
-  app.use(cookieParser());
+  app.use(cookieParser());  // Middleware cho cookie parser
   await app.listen(process.env.PORT ?? 3000);
 }
 
